a72d15b6223b63878556eb0583283d6822f3e805
Tom Farmer <farmergeddon@gmail.com>
2019-07-15 21:45:22 +0100

    Fixed multithreaded scan line interlace

--- src/triangle-rasterizer-demo/trd/trdRenderManager.cpp
+++ src/triangle-rasterizer-demo/trd/trdRenderManager.cpp
@@ -1,4 +1,5 @@
 #include "trdRenderManager.hpp"
+#include <thread>
 
 trd::RenderManager::RenderManager(const Settings& settings) :
 	m_settings(settings)
@@ -7,37 +8,37 @@ trd::RenderManager::RenderManager(const Settings& settings) :
 	m_meshMap.add("test.obj", m_textureMap);
 
 	m_models.emplace_back("test.obj", m_meshMap, Vector3(), Vector2());
-
-	initThreads(settings.getNumThreads());
 }
 
 void trd::RenderManager::draw(const Camera& camera, tr::ColorBuffer& colorBuffer, tr::DepthBuffer& depthBuffer)
 {
-	if (size_t(m_settings.getNumThreads()) != m_threads.size())
+	std::list<std::thread> threads;
+
+	for (size_t i = 0; i < m_settings.getNumThreads(); ++i)
 	{
-		initThreads(size_t(m_settings.getNumThreads()));
+		threads.emplace_back(&RenderManager::renderThread, this, i, std::ref(camera), std::ref(colorBuffer), std::ref(depthBuffer));
 	}
 
-	for (RenderThread& thread : m_threads)
+	for (std::thread& thread : threads)
 	{
-		thread.drawFrame(camera, colorBuffer, depthBuffer);
-	}
-
-	for (RenderThread& thread : m_threads)
-	{
-		while (!thread.isFinished())
-		{
-			std::this_thread::sleep_for(std::chrono::milliseconds(1));
-		}
+		thread.join();
 	}
 }
 
-void trd::RenderManager::initThreads(const size_t numThreads)
+void trd::RenderManager::renderThread(const size_t threadIndex, const Camera& camera, tr::ColorBuffer& colorBuffer, tr::DepthBuffer& depthBuffer) const
 {
-	m_threads.clear();
+	tr::Rasterizer<Shader> rasterizer;
+	Shader                 shader;
 
-	for (size_t i = 0; i < numThreads; ++i)
+	rasterizer.setInterlace(threadIndex, m_settings.getNumThreads());
+	rasterizer.setCullFaceMode(tr::CullFaceMode::Back);
+	rasterizer.setPrimitive(tr::Primitive::Triangles);
+	rasterizer.setDepthTest(true);
+	rasterizer.setTextureMode(m_settings.getTextureMode());
+	shader.setRenderMode(m_settings.getRenderMode());
+
+	for (const Model& model : m_models)
 	{
-		m_threads.emplace_back(m_settings, m_models, i, numThreads);
+		model.draw(camera, rasterizer, shader, colorBuffer, depthBuffer);
 	}
 }
--- src/triangle-rasterizer-demo/trd/trdRenderManager.hpp
+++ src/triangle-rasterizer-demo/trd/trdRenderManager.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
-#include "trdRenderThread.hpp"
+#include "trdCamera.hpp"
+#include "trdSettings.hpp"
+#include "trColorBuffer.hpp"
+#include "trDepthBuffer.hpp"
+#include "trdTextureMap.hpp"
+#include "trdModel.hpp"
 
 namespace trd
 {
@@ -12,13 +17,12 @@ namespace trd
 		void                     draw(const Camera& camera, tr::ColorBuffer& colorBuffer, tr::DepthBuffer& depthBuffer);
 
 	private:
-		void                     initThreads(const size_t numThreads);
+		void                     renderThread(const size_t threadIndex, const Camera& camera, tr::ColorBuffer& colorBuffer, tr::DepthBuffer& depthBuffer) const;
 
 	private:
 		const Settings&          m_settings;
 		TextureMap               m_textureMap;
 		MeshMap                  m_meshMap;
 		tf::Vector<Model>        m_models;
-		std::list<RenderThread> m_threads;
 	};
 }
